eb5c5c4d4682d3a035babf9c65d707b78106df2c
alirezakh <alirezak@uga.edu>
2015-02-01 19:32:38 -0500

    Update Genre.java
    Attributes, accessor and mutator functions

--- Server/src/Genre.java
+++ Server/src/Genre.java
@@ -1,4 +1,32 @@
-package MoviePickRESTfulService
+package MoviePickRESTfulService;
 
 public class Genre{
+  private String genreId;
+  private String name;
+  
+  public Genre(){
+    this.genreId = "";
+    this.name = "";
+  }
+  
+  public Genre(String genreId, String name){
+    this.genreId = genreId;
+    this.name = name;
+  }
+  
+  public String getGenreId(){
+    return this.genreId;
+  }
+  
+  public void setGenreId(String id){
+    this.genreId = id;
+  }
+  
+  public String getName(){
+    return this.name;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
 }
