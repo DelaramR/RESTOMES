4d21cae0934ddaf277ff92a033a19da2fbd417b1
alirezakh <alirezak@uga.edu>
2015-02-03 11:53:45 -0500

    Update Genre.java

--- Server/src/Genre.java
+++ Server/src/Genre.java
@@ -1,25 +1,14 @@
 package MoviePickRESTfulService;
 
 public class Genre{
-  private String genreId;
   private String name;
   
   public Genre(){
-    this.genreId = "";
     this.name = "";
   }
   
-  public Genre(String genreId, String name){
-    this.genreId = genreId;
+  public Genre(String name){
     this.name = name;
-  }
-  
-  public String getGenreId(){
-    return this.genreId;
-  }
-  
-  public void setGenreId(String id){
-    this.genreId = id;
   }
   
   public String getName(){
