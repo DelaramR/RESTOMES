6202e46daf879ae122305ea3f998308a36b4226e
alirezakh <alirezak@uga.edu>
2015-02-03 14:45:43 -0500

    Update Theater.java

--- Server/src/Theater.java
+++ Server/src/Theater.java
@@ -1,28 +1,17 @@
 package MoviePickRESTfulService;
 
 public class Theater{
-  private int theaterId;
   private String theaterName;
   private String theaterAddress;
   
   public Theater(){
-    this.theaterId = 0;
     this.theaterName = "";
     this.theaterAddress = "";
   }
   
-  public Theater(int id, String name, String address){
-    this.theaterId = id;
+  public Theater(String name, String address){
     this.theaterName = name;
     this.theaterAddress = address;
-  }
-  
-  public int getTheaterId(){
-    return this.theaterId;
-  }
-  
-  public void setTheaterId(int id){
-    this.theaterId = id;
   }
   
   public String getTheaterName(){
