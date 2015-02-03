359b41482cc4a31eeba6c437058bbce9aecab51a
alirezakh <alirezak@uga.edu>
2015-02-03 15:11:34 -0500

    Update MovieResource.java

--- Server/src/MovieResource.java
+++ Server/src/MovieResource.java
@@ -1,5 +1,13 @@
 package MoviePickRESTfulService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,13 +22,58 @@ import javax.ws.rs.core.StreamingOutput;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.jboss.resteasy.spi.NoLogWebApplicationException;
+
 @Path("/movie")
 public class MovieResource{
+  
+  private static final Map<Integer, Movie> movieDB = new HashMap<Integer, Movie>();
+  
   @POST
-  @Consumes( MediaType.APPLICATION_XML )
-  public Response createEntryXML( InputStream is )
-  {
+  @Consumes( MediaType.APPLICATION_JSON )
+  public Response createEntryJSON( Movie movie ){
     System.out.println( "MovieResource.createEntry" );
+    Integer id = movieDB.size() + 1;
+    movieDB.put(id, movie);
     return Response.created( URI.create("/movie/" + id) ).build();
+  }
+  
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Map<Integer, Movie> returnListJSON(){
+    return movieDB;
+  }
+  
+  @GET
+  @Path( "{id}" )
+  @Produces(MediaType.APPLICATION_JSON)
+  public Movie getEntryJSON(@PathParam("id") Integer id){
+    final Movie movie = mvoieDB.get(id);
+    if(movie == null){
+      throw new NoLogWebApplicationException(Response.Status.NOT_FOUND);
+    }
+    return movie;
+  }
+  
+  @PUT
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response updateMovieJSON(@PathParam("id") Integer id, Movie movie){
+    Movie current = movieDB.get(id);
+    if(current == null){
+      throw new NoLogWebApplicationException(Response.Status.NOT_FOUND);
+    }
+    current.setMovieTitle(movie.getMovieTitle());
+    current.setGenre(movie.getGenre());
+    current.setRate((current.getRate() + movie.getRate())/2);
+    
+    return Response.ok().build();
+  }
+  
+  @DELETE
+  @Path("{id}")
+  public Response deleteMovie(@PathParam("id") Integer id){
+     movieDB.remove(id);
+     return Response.ok().build();
   }
 }
