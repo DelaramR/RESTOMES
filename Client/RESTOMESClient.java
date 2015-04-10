104d453d35774bada6a52c76611959eee1521962
alirezakh <alirezak@uga.edu>
2015-04-09 22:54:03 -0400

    Update RESTOMESClient.java

--- Client/RESTOMESClient.java
+++ Client/RESTOMESClient.java
@@ -41,6 +41,7 @@ public class RESTOMESClient{
 		try {
 			String ontologyName = args[0].substring(args[0].lastIndexOf("/") + 1);
 			String ontology = RESTOMESClient.CreateJsonFile(args[0], ontologyName);
+			System.out.print(ontology);
 			System.out.println( "Creating an ontology (JSON): " + ontologyName );
 			ResteasyClient client = new ResteasyClientBuilder().build();
 			ResteasyWebTarget target = client.target( "http://uml.cs.uga.edu:8080/RESTOMES/rest/ontology" );
