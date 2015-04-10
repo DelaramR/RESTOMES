92a2437dba6b644ad24bc7502dc3fee975e7eb74
alirezakh <alirezak@uga.edu>
2015-04-09 20:22:51 -0400

    Update Ontology.java

--- Server/src/RESTOMES/Ontology.java
+++ Server/src/RESTOMES/Ontology.java
@@ -10,7 +10,7 @@ public class Ontology{
 	private Map<Integer, OntologyClass> ontologyClasses;
 	private Map<Integer, ObjectProperty> objectProperties;
 	private Map<Integer, DataProperty> dataProperties;
-	
+
 	public Ontology(){
 		this.filePath = "";
 		this.name = "";
@@ -19,51 +19,51 @@ public class Ontology{
 		this.dataProperties = new HashMap<Integer, DataProperty>();
 	}
 
-    public Ontology(String file, String name, Map<Integer, OntologyClass> classes, Map<Integer, ObjectProperty> oProperties, Map<Integer, DataProperty> dProperties){	
-    	this.filePath = file;
-    	this.name = name;
-    	this.ontologyClasses = classes;
-    	this.objectProperties = oProperties;
-    	this.dataProperties = dProperties;
-    }
+	public Ontology(String file, String name, Map<Integer, OntologyClass> classes, Map<Integer, ObjectProperty> oProperties, Map<Integer, DataProperty> dProperties){	
+		this.filePath = file;
+		this.name = name;
+		this.ontologyClasses = classes;
+		this.objectProperties = oProperties;
+		this.dataProperties = dProperties;
+	}
 
 	public String getFilePath(){
 		return this.filePath;
 	}
-	
+
 	public void setFilePath(String path){
 		this.filePath = path;
 	}
 
-    public String getName(){
-    	return this.name;
-    }
+	public String getName(){
+		return this.name;
+	}
 
-    public void setName(String name){
-    	this.name = name;
-    }
+	public void setName(String name){
+		this.name = name;
+	}
 
-    public Map<Integer, OntologyClass> getOntologyClasses(){
-    	return this.ontologyClasses;
-    }
+	public Map<Integer, OntologyClass> getOntologyClasses(){
+		return this.ontologyClasses;
+	}
 
-    public void setOntologyClasses(Map<Integer, OntologyClass> classes){
-    	this.ontologyClasses = classes;
-    }
+	public void setOntologyClasses(Map<Integer, OntologyClass> classes){
+		this.ontologyClasses = classes;
+	}
 
-    public Map<Integer, ObjectProperty> getObjectProperties(){
-    	return this.objectProperties;
-    }
+	public Map<Integer, ObjectProperty> getObjectProperties(){
+		return this.objectProperties;
+	}
 
-    public void setObjectProperties(Map<Integer, ObjectProperty> oProperties){
-    	this.objectProperties = oProperties;
-    }
+	public void setObjectProperties(Map<Integer, ObjectProperty> oProperties){
+		this.objectProperties = oProperties;
+	}
 
-    public Map<Integer, DataProperty> getDataProperties(){
-    	return this.dataProperties;
-    }
+	public Map<Integer, DataProperty> getDataProperties(){
+		return this.dataProperties;
+	}
 
-    public void setDataProperties(Map<Integer, DataProperty> dProperties){
-    	this.dataProperties = dProperties;
-    }
+	public void setDataProperties(Map<Integer, DataProperty> dProperties){
+		this.dataProperties = dProperties;
+	}
 }
