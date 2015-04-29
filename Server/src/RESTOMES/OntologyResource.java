ecf621223dd15e55df74b2cf87dcd58c1e74f676
DelaramR <delaram.ra@gmail.com>
2015-04-29 18:47:15 -0400

    Update OntologyResource.java

--- Server/src/RESTOMES/OntologyResource.java
+++ Server/src/RESTOMES/OntologyResource.java
@@ -17,9 +17,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
@@ -39,8 +36,6 @@ public class OntologyResource{
   public static final Map<Integer, Ontology> ontologyDB = new HashMap<Integer, Ontology>();
   public static final String QUERY_NAMESPACES = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\r\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\r\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\nPREFIX fn: <http://www.w3.org/2005/xpath-functions#>\r\n";
   
-  @Context
-  UriInfo uri;
   /**
      * Register a new ontology entry using a JSON representation.
      * @param ontology the new ontology object data; this will be converted to POJO by a JSON provider (Jackson)
@@ -73,10 +68,34 @@ public class OntologyResource{
         }
       }
       Map<Integer, OntologyClass> ontologyClasses = new HashMap<Integer, OntologyClass>();
-      String queryString = QUERY_NAMESPACES + "select distinct ?class where { ?class a owl:Class.}";      
+      Map<String, Integer> classNameIDMap = new HashMap<String, Integer>();
+      String queryString = QUERY_NAMESPACES + "select distinct ?class where { ?class a owl:Class.}";
+      Map<Integer, ObjectProperty> objectProperties = new HashMap<Integer, ObjectProperty>();
+      String queryString1 = QUERY_NAMESPACES + "SELECT distinct " + 
+      "?ObjectProperty(group_concat(distinct ?range;separator=" | ") as ?ranges)" + 
+      " (group_concat(distinct ?domain;separator=" | ") as ?domains)" +
+	"WHERE {" + 
+		"?ObjectProperty a owl:ObjectProperty." +
+		"optional { ?ObjectProperty rdfs:domain ?domain. }" +
+		"optional { ?ObjectProperty rdfs:range ?range. }" +		
+		"}" +
+	"group by ?ObjectProperty";" 
+
+      Map<Integer, DataProperty> dataProperties = new HashMap<Integer, DataProperty>();
+      String queryString2 = QUERY_NAMESPACES + "SELECT distinct " + 
+      "?DatatypeProperty(group_concat(distinct ?range;separator=" | ") as ?ranges)" + 
+      " (group_concat(distinct ?domain;separator=" | ") as ?domains)" +
+	"WHERE {" + 
+		"?DatatypeProperty a owl:DatatypeProperty." +
+		"optional { ?DatatypeProperty rdfs:domain ?domain. }" +
+		"optional { ?DatatypeProperty rdfs:range ?range. }" +		
+		"}" +
+	"group by ?DatatypeProperty";" 	
+      
       try{
       Model model = ModelFactory.createDefaultModel();
       model = model.read(ontologyUrl);
+      ///Building metadata of Classes
       Query query = QueryFactory.create(queryString);
       QueryExecution queryExec = QueryExecutionFactory.create(query, model);
       ResultSet classes = queryExec.execSelect();
@@ -87,6 +106,65 @@ public class OntologyResource{
 		 int key = ontologyClasses.size() + 1;
 		 OntologyClass ontologyClass = new OntologyClass(value);
 		 ontologyClasses.put(key, ontologyClass);
+	    }
+      ///Building metadata of Object Properties	    
+      Query query1 = QueryFactory.create(queryString1);
+      QueryExecution queryExec1 = QueryExecutionFactory.create(query1, model);
+      ResultSet objectProperties_result = queryExec1.execSelect();
+      while(objectProperties_result.hasNext()){
+		 QuerySolution entity = objectProperties_result.next();
+		 
+		 String domains = entity.get("domains").toString();
+		 String[] domains_tokens = domains.split(" | ");
+		 ArrayList<OntologyClass> domain_list = new ArrayList<OntologyClass>();
+		 for(int i = 0; i < domains_tokens.length; i++){
+		 	String domain_name = domains_tokens[i].substring(domains_tokens[i].lastIndexOf("/") + 1);	
+		 	if((Integer id = classNameIDMap.get(domain_name)) != null)
+		 		if((OntologyClass oc = ontologyClasses.get(id)) != null)
+		 			domain_list.put(oc);
+		 }
+		 String ranges = entity.get("ranges").toString();
+		 String[] range_tokens = range.split(" | ");
+		 ArrayList<OntologyClass> range_list = new ArrayList<OntologyClass>();
+		 for(int i = 0; i < ranges_tokens.length; i++){
+		 	String range_name = ranges_tokens[i].substring(ranges_tokens[i].lastIndexOf("/") + 1);	
+		 	if((Integer id = classNameIDMap.get(range_name)) != null)
+		 		if((OntologyClass oc = ontologyClasses.get(id)) != null)
+		 			range_list.put(oc);
+		 }
+		 
+		 String property_name = entity.get("ObjectProperty").toString();
+		 property_name = property_name.substring(property_name.lastIndexOf("/") + 1);
+		 
+		 int key = objectProperties.size() + 1;
+		 ObjectProperty objectProperty = new ObjectProperty(domain_list, range_list, property_name);
+		 objectProperties.put(key, ObjectProperty);
+
+      ///Building metadata of Data Properties	    
+      Query query2 = QueryFactory.create(queryString2);
+      QueryExecution queryExec2 = QueryExecutionFactory.create(query2, model);
+      ResultSet dataProperties_result = queryExec2.execSelect();
+      while(dataProperties_result.hasNext()){
+		 QuerySolution entity = dataProperties_result.next();
+		 
+		 String domains = entity.get("domains").toString();
+		 String[] domains_tokens = domains.split(" | ");
+		 ArrayList<OntologyClass> domain_list = new ArrayList<OntologyClass>();
+		 for(int i = 0; i < domains_tokens.length; i++){
+		 	String domain_name = domains_tokens[i].substring(domains_tokens[i].lastIndexOf("/") + 1);	
+		 	if((Integer id = classNameIDMap.get(domain_name)) != null)
+		 		if((OntologyClass oc = ontologyClasses.get(id)) != null)
+		 			domain_list.put(oc);
+		 }
+		 String ranges = entity.get("ranges").toString();
+		 ranges = ranges.substring(ranges.lastIndexOf("/") + 1);
+		 String property_name = entity.get("DataTypeProperty").toString();
+		 property_name = property_name.substring(property_name.lastIndexOf("/") + 1);
+		 
+		 int key = dataProperties.size() + 1;
+		 DataProperty dataProperty = new DataProperty(domain_list, ranges, property_name);
+		 dataProperties.put(key, DataProperty);
+
 	    }
       }catch(Exception ex){
 	System.out.println(ex.toString());
@@ -108,41 +186,9 @@ public class OntologyResource{
      * @return map object of all ontologies; it will be converted to JSON using a JSON provider (Jackson)
      */
   @GET
-  @Produces(MediaType.TEXT_HTML)
-  public String returnOntologyListJSON(){
-  	String html = "<html>\r\n" +
-  		"<head>\r\n" + 
-  		"</head>\r\n" + 
-  		"<body>" + 
-  		"<table>\r\n" +
-  		"<tr>\r\n" +
-  		"<td><h4>RESTful Ontology Metadata Extractor/Storage System</h4></td>\r\n" +
-		"</tr>\r\n" +
-		"</table>\r\n" +
-		"<table>\r\n" +
-		"<tr>\r\n" + 
-		"<td>\r\n" +
-		"<div>\r\n" +
-		"Ontology URI: <input type=\"text\" id=\"uri\"><br>\r\n" +
-		"<br>\r\n" +
-		"<button id=\"btnLogin\">Upload</button>\r\n" +
-		"</div>\r\n" +
-		"<div align=\"center\" id=\"result\">\r\n";
-	for (Map.Entry<Integer, Ontology> entry : ontologyDB.entrySet()){
-		UriBuilder ub = uri.getAbsolutePathBuilder();
-            	URI userUri = ub.path(entry.getKey().toString()).build();
-		String value = userUri.toString();
-		html += "<a href=" + value + ">" + value + "</a>\r\n";
-	}
-	html += "</div>\r\n" +
-		"</td>\r\n" +
-		"</tr>\r\n" +
-		"</table>\r\n" +
-		"</body>\r\n" +
-		"</html>";
-	
-	
-	return html;
+  @Produces(MediaType.APPLICATION_JSON)
+  public Map<Integer, Ontology> returnOntologyListJSON(){
+    return ontologyDB;
   }
   
      /**
