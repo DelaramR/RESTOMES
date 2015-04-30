62f38dac75554f9a6e3ac987f860eda98b55ffaf
alirezakh <alirezak@uga.edu>
2015-04-30 00:59:41 -0400

    Update OntologyResource.java

--- Server/src/RESTOMES/OntologyResource.java
+++ Server/src/RESTOMES/OntologyResource.java
@@ -119,7 +119,7 @@ public class OntologyResource{
 		 
 		 //String property_name = entity.get("?ObjectProperty").toString();
 		 //property_name = property_name.substring(property_name.lastIndexOf("/") + 1);
-		 String domains = entity.getResource("domains").toString();
+		 RDFNode domains = entity.get("domains");
 		 //if(objectPropertyIDMap.get(property_name) != null){
 		 //	Integer id = objectPropertyIDMap.get(property_name);
 		 //	String domains = entity.get("domains").toString();	
