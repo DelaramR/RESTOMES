5a82b74dc0f5da83d52a991f52b891461d054aa2
alirezakh <alirezak@uga.edu>
2015-04-30 21:51:16 -0400

    Update OntologyResource.java

--- Server/src/RESTOMES/OntologyResource.java
+++ Server/src/RESTOMES/OntologyResource.java
@@ -836,7 +836,7 @@ public class OntologyResource{
     	for (Map.Entry<Integer, ObjectProperty> entry1 : ontology.getObjectProperties().entrySet()){
     		if(entry1.getValue().getProperty().compareTo(entry.getProperty()) == 0){
     			UriBuilder ub = uri.getBaseUriBuilder();
-    			URI userUri = ub.path("ontology/" + id + "/objectproperty/" + entry1.getKey()).build();
+    			URI userUri = ub.path("ontology/" + oid + "/objectproperty/" + entry1.getKey()).build();
     			String value = userUri.toString();
     			html += "<a href=" + value + ">" + entry.getProperty() + "</a><br>\r\n";
     			break;
