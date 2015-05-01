0f516e906d20f5ae103ac61364151f9c086aca62
alirezakh <alirezak@uga.edu>
2015-04-30 22:17:19 -0400

    Update OntologyClass.java

--- Server/src/RESTOMES/OntologyClass.java
+++ Server/src/RESTOMES/OntologyClass.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 public class OntologyClass{
     private String className;
     private ArrayList<OntologyClass> subClassOf;
+    private ArrayList<OntologyClass> disjointWith;
     
     public OntologyClass(){
 	this.className = "";
 	this.subClassOf = new ArrayList<OntologyClass>();
+	this.disjointWith = new ArrayList<OntologyClass>();
     }
 
     public OntologyClass(String name){
 	this.className = name;
 	this.subClassOf = new ArrayList<OntologyClass>();
+	this.disjointWith = new ArrayList<OntologyClass>();
     }
 
     public String getClassName(){
@@ -30,5 +33,13 @@ public class OntologyClass{
     
     public void setSubClassOf(ArrayList<OntologyClass> subClassOf){
     	this.subClassOf = subClassOf;
+    }
+    
+    public ArrayList<OntologyClass> getDisjointWith(){
+    	return this.disjointWith;
+    }
+    
+    public void setSubClassOf(ArrayList<OntologyClass> disjointWith){
+    	this.disjointWith = disjointWith;
     }
 }
